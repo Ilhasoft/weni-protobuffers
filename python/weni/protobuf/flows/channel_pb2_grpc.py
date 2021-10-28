fb4f864188c894a171068890382e5cac007b6b3b
Sandro Meireles <sandro.meirelesdev@gmail.com>
2021-10-28 14:55:57 -0300

    feat: Add channel release endpoint

--- python/weni/protobuf/flows/channel_pb2_grpc.py
+++ python/weni/protobuf/flows/channel_pb2_grpc.py
@@ -2,7 +2,69 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from weni.protobuf.flows import channel_pb2 as weni_dot_protobuf_dot_flows_dot_channel__pb2
+
+
+class ChannelControllerStub(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def __init__(self, channel):
+        """Constructor.
+
+        Args:
+            channel: A grpc.Channel.
+        """
+        self.Destroy = channel.unary_unary(
+                '/weni.flows.channel.ChannelController/Destroy',
+                request_serializer=weni_dot_protobuf_dot_flows_dot_channel__pb2.ChannelDestroyRequest.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                )
+
+
+class ChannelControllerServicer(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def Destroy(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+
+def add_ChannelControllerServicer_to_server(servicer, server):
+    rpc_method_handlers = {
+            'Destroy': grpc.unary_unary_rpc_method_handler(
+                    servicer.Destroy,
+                    request_deserializer=weni_dot_protobuf_dot_flows_dot_channel__pb2.ChannelDestroyRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            ),
+    }
+    generic_handler = grpc.method_handlers_generic_handler(
+            'weni.flows.channel.ChannelController', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler,))
+
+
+ # This class is part of an EXPERIMENTAL API.
+class ChannelController(object):
+    """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def Destroy(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/weni.flows.channel.ChannelController/Destroy',
+            weni_dot_protobuf_dot_flows_dot_channel__pb2.ChannelDestroyRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
 
 class WeniWebChatControllerStub(object):
