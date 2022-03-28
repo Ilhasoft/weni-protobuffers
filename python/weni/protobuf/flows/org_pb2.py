60ce5ec8193929287eadca3853824c273e30f1b0
Paulo Abreu <pauloabreucoder@gmail.com>
2022-03-28 17:16:45 -0300

    add is_active to OrgUpdateRequest

--- python/weni/protobuf/flows/org_pb2.py
+++ python/weni/protobuf/flows/org_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1dweni/protobuf/flows/org.proto\x12\x0eweni.flows.org\x1a\x1bgoogle/protobuf/empty.proto\"y\n\x03Org\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04uuid\x18\x03 \x01(\t\x12\x10\n\x08timezone\x18\x04 \x01(\t\x12\x13\n\x0b\x64\x61te_format\x18\x05 \x01(\t\x12#\n\x05users\x18\x06 \x03(\x0b\x32\x14.weni.flows.org.User\"s\n\x04User\x12\n\n\x02id\x18\x01 \x01(\x05\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x10\n\x08username\x18\x03 \x01(\t\x12\x12\n\nfirst_name\x18\x04 \x01(\t\x12\x11\n\tlast_name\x18\x05 \x01(\t\x12\x17\n\x0fpermission_type\x18\x06 \x01(\t\"$\n\x0eOrgListRequest\x12\x12\n\nuser_email\x18\x01 \x01(\t\"F\n\x10OrgCreateRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08timezone\x18\x02 \x01(\t\x12\x12\n\nuser_email\x18\x03 \x01(\t\"\"\n\x12OrgRetrieveRequest\x12\x0c\n\x04uuid\x18\x01 \x01(\t\"5\n\x11OrgDestroyRequest\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x12\n\nuser_email\x18\x02 \x01(\t\"\xba\x03\n\x10OrgUpdateRequest\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x18\n\x0bmodified_by\x18\x02 \x01(\tH\x00\x88\x01\x01\x12\x11\n\x04name\x18\x03 \x01(\tH\x01\x88\x01\x01\x12\x15\n\x08timezone\x18\x04 \x01(\tH\x02\x88\x01\x01\x12\x18\n\x0b\x64\x61te_format\x18\x05 \x01(\tH\x03\x88\x01\x01\x12\x11\n\x04plan\x18\x06 \x01(\tH\x04\x88\x01\x01\x12\x15\n\x08plan_end\x18\x07 \x01(\tH\x05\x88\x01\x01\x12\x12\n\x05\x62rand\x18\x08 \x01(\tH\x06\x88\x01\x01\x12\x14\n\x07is_anon\x18\t \x01(\x08H\x07\x88\x01\x01\x12\x1a\n\ris_multi_user\x18\n \x01(\x08H\x08\x88\x01\x01\x12\x19\n\x0cis_multi_org\x18\x0b \x01(\x08H\t\x88\x01\x01\x12\x19\n\x0cis_suspended\x18\x0c \x01(\x08H\n\x88\x01\x01\x42\x0e\n\x0c_modified_byB\x07\n\x05_nameB\x0b\n\t_timezoneB\x0e\n\x0c_date_formatB\x07\n\x05_planB\x0b\n\t_plan_endB\x08\n\x06_brandB\n\n\x08_is_anonB\x10\n\x0e_is_multi_userB\x0f\n\r_is_multi_orgB\x0f\n\r_is_suspended2\xe5\x02\n\rOrgController\x12?\n\x04List\x12\x1e.weni.flows.org.OrgListRequest\x1a\x13.weni.flows.org.Org\"\x00\x30\x01\x12\x41\n\x06\x43reate\x12 .weni.flows.org.OrgCreateRequest\x1a\x13.weni.flows.org.Org\"\x00\x12\x45\n\x08Retrieve\x12\".weni.flows.org.OrgRetrieveRequest\x1a\x13.weni.flows.org.Org\"\x00\x12\x41\n\x06Update\x12 .weni.flows.org.OrgUpdateRequest\x1a\x13.weni.flows.org.Org\"\x00\x12\x46\n\x07\x44\x65stroy\x12!.weni.flows.org.OrgDestroyRequest\x1a\x16.google.protobuf.Empty\"\x00\x62\x06proto3'
+  serialized_pb=b'\n\x1dweni/protobuf/flows/org.proto\x12\x0eweni.flows.org\x1a\x1bgoogle/protobuf/empty.proto\"y\n\x03Org\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04uuid\x18\x03 \x01(\t\x12\x10\n\x08timezone\x18\x04 \x01(\t\x12\x13\n\x0b\x64\x61te_format\x18\x05 \x01(\t\x12#\n\x05users\x18\x06 \x03(\x0b\x32\x14.weni.flows.org.User\"s\n\x04User\x12\n\n\x02id\x18\x01 \x01(\x05\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x10\n\x08username\x18\x03 \x01(\t\x12\x12\n\nfirst_name\x18\x04 \x01(\t\x12\x11\n\tlast_name\x18\x05 \x01(\t\x12\x17\n\x0fpermission_type\x18\x06 \x01(\t\"$\n\x0eOrgListRequest\x12\x12\n\nuser_email\x18\x01 \x01(\t\"F\n\x10OrgCreateRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08timezone\x18\x02 \x01(\t\x12\x12\n\nuser_email\x18\x03 \x01(\t\"\"\n\x12OrgRetrieveRequest\x12\x0c\n\x04uuid\x18\x01 \x01(\t\"5\n\x11OrgDestroyRequest\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x12\n\nuser_email\x18\x02 \x01(\t\"\xe0\x03\n\x10OrgUpdateRequest\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x18\n\x0bmodified_by\x18\x02 \x01(\tH\x00\x88\x01\x01\x12\x11\n\x04name\x18\x03 \x01(\tH\x01\x88\x01\x01\x12\x15\n\x08timezone\x18\x04 \x01(\tH\x02\x88\x01\x01\x12\x18\n\x0b\x64\x61te_format\x18\x05 \x01(\tH\x03\x88\x01\x01\x12\x11\n\x04plan\x18\x06 \x01(\tH\x04\x88\x01\x01\x12\x15\n\x08plan_end\x18\x07 \x01(\tH\x05\x88\x01\x01\x12\x12\n\x05\x62rand\x18\x08 \x01(\tH\x06\x88\x01\x01\x12\x14\n\x07is_anon\x18\t \x01(\x08H\x07\x88\x01\x01\x12\x1a\n\ris_multi_user\x18\n \x01(\x08H\x08\x88\x01\x01\x12\x19\n\x0cis_multi_org\x18\x0b \x01(\x08H\t\x88\x01\x01\x12\x19\n\x0cis_suspended\x18\x0c \x01(\x08H\n\x88\x01\x01\x12\x16\n\tis_active\x18\r \x01(\x08H\x0b\x88\x01\x01\x42\x0e\n\x0c_modified_byB\x07\n\x05_nameB\x0b\n\t_timezoneB\x0e\n\x0c_date_formatB\x07\n\x05_planB\x0b\n\t_plan_endB\x08\n\x06_brandB\n\n\x08_is_anonB\x10\n\x0e_is_multi_userB\x0f\n\r_is_multi_orgB\x0f\n\r_is_suspendedB\x0c\n\n_is_active2\xe5\x02\n\rOrgController\x12?\n\x04List\x12\x1e.weni.flows.org.OrgListRequest\x1a\x13.weni.flows.org.Org\"\x00\x30\x01\x12\x41\n\x06\x43reate\x12 .weni.flows.org.OrgCreateRequest\x1a\x13.weni.flows.org.Org\"\x00\x12\x45\n\x08Retrieve\x12\".weni.flows.org.OrgRetrieveRequest\x1a\x13.weni.flows.org.Org\"\x00\x12\x41\n\x06Update\x12 .weni.flows.org.OrgUpdateRequest\x1a\x13.weni.flows.org.Org\"\x00\x12\x46\n\x07\x44\x65stroy\x12!.weni.flows.org.OrgDestroyRequest\x1a\x16.google.protobuf.Empty\"\x00\x62\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_empty__pb2.DESCRIPTOR,])
 
@@ -402,6 +402,13 @@ _ORGUPDATEREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='is_active', full_name='weni.flows.org.OrgUpdateRequest.is_active', index=12,
+      number=13, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -468,9 +475,14 @@ _ORGUPDATEREQUEST = _descriptor.Descriptor(
       index=10, containing_type=None,
       create_key=_descriptor._internal_create_key,
     fields=[]),
+    _descriptor.OneofDescriptor(
+      name='_is_active', full_name='weni.flows.org.OrgUpdateRequest._is_active',
+      index=11, containing_type=None,
+      create_key=_descriptor._internal_create_key,
+    fields=[]),
   ],
   serialized_start=520,
-  serialized_end=962,
+  serialized_end=1000,
 )
 
 _ORG.fields_by_name['users'].message_type = _USER
@@ -507,6 +519,9 @@ _ORGUPDATEREQUEST.fields_by_name['is_multi_org'].containing_oneof = _ORGUPDATERE
 _ORGUPDATEREQUEST.oneofs_by_name['_is_suspended'].fields.append(
   _ORGUPDATEREQUEST.fields_by_name['is_suspended'])
 _ORGUPDATEREQUEST.fields_by_name['is_suspended'].containing_oneof = _ORGUPDATEREQUEST.oneofs_by_name['_is_suspended']
+_ORGUPDATEREQUEST.oneofs_by_name['_is_active'].fields.append(
+  _ORGUPDATEREQUEST.fields_by_name['is_active'])
+_ORGUPDATEREQUEST.fields_by_name['is_active'].containing_oneof = _ORGUPDATEREQUEST.oneofs_by_name['_is_active']
 DESCRIPTOR.message_types_by_name['Org'] = _ORG
 DESCRIPTOR.message_types_by_name['User'] = _USER
 DESCRIPTOR.message_types_by_name['OrgListRequest'] = _ORGLISTREQUEST
@@ -574,8 +589,8 @@ _ORGCONTROLLER = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=965,
-  serialized_end=1322,
+  serialized_start=1003,
+  serialized_end=1360,
   methods=[
   _descriptor.MethodDescriptor(
     name='List',
